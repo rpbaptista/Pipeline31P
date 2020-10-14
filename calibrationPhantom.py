86ab51bb2219a286ed8f646c338cc356986d5317
Renata Baptista <r.baptista@poli.ufrj.br>
2020-10-14 14:46:34 +0200

    Update to script tpu-rad-comparison

---
+++ calibrationPhantom.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 15:33:54 2020
+
+@author: RP258738, renata.porciunculabaptista@cea.fr
+
+This script FID .dat
+
+Dependencies:
+ - spm standalone + MCR https://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/MCR/glnxa64/
+ - or Maltab + SPM
+"""
+#import numpy as np
+#import sys, os
+
+#sys.path.append(os.path.join(sys.path[0],'./parameters/'))
+#from parameters.initialization_phantom import INITIALIZATION
+
+#phantom = INITIALIZATION['phantom_uniform']
+print("Hello world")
