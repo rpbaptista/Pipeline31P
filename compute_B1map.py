da461357458ddb6cad362281647f2090a124db5e
rpbaptista <=>
2021-01-29 10:27:19 +0100

    test phantom_2

--- compute_B1map.py
+++ compute_B1map.py
@@ -14,12 +14,13 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import nibabel as nib 
 from scipy import stats
+from skimage.restoration import denoise_nl_means, estimate_sigma
 
 # My libraries
 from utils_own.b1_mapping import *
 from utils import openArrayImages
 
-init = INITIALIZATION['b1_database'] 
+init = INITIALIZATION['b1_database_phantom'] 
 
 
 # Simulating data
@@ -30,23 +31,25 @@ N_iterations = 1000
 # True values
 # Noise
 
-alpha_0 = np.linspace(5, 30, num=15) # np.array([25,35,45] )
+"""alpha_0 = np.linspace(0, 45, num=15) # np.array([25,35,45] )
 results_monte_carlo = np.zeros((alpha_0.size,N_iterations))
 for j in range(alpha_0.size):
     S_alpha = equation_B1(init['T1'] ,init['TR'],get_sequence_alpha(alpha_0[j] ) , S0)
-    print(S_alpha)
-    std = 0.1*S_alpha[0]  
-    mean = 1*S_alpha[0]  
+    std = 0.2*np.max(S_alpha ) 
+    mean = 1*np.max(S_alpha ) 
     noise =  std * np.random.randn(S_alpha.size,N_iterations) + mean
 
     for i in range(N_iterations):
         y_obs = S_alpha +noise[:,i]
+        x_values =[ init['T1'] ,init['TR']]
+        for k in range(len(y_obs)):
+            x_values.append(y_obs[k] )
         pop, _ = curve_fit(function_B1_double,
-                                [init['T1'] ,init['TR']],
+                                x_values,
                                 y_obs,
                                 maxfev=600)
                                 
-        results_monte_carlo[j,i], a = pop #
+        results_monte_carlo[j,i] = pop # , a = pop #
 #plt.plot(get_sequence_alpha(results_monte_carlo).T)   
 plt.plot(alpha_0, alpha_0, '-', label='Ground truth' )#
 plt.plot(alpha_0, np.mean(results_monte_carlo,axis=1), '-', label='Mean' )#
@@ -55,42 +58,84 @@ plt.plot(alpha_0, np.mean(results_monte_carlo,axis=1)-np.std(results_monte_carlo
 plt.legend()
 #plt.plot(get_sequence_alpha(alpha_0), results_monte_carlo)
 plt.show()
-
-
-
 """
+
+#idx_courone = [8,10,13] 
+idx_courone = [14,10,13] 
+
+
 # REAL DATA
 keys_sub = search_keys_sub(init)
 for sub in keys_sub:
     SFdata = openArrayImages(init[sub])
     shape = SFdata.shape
     # FITT
-    y_values_alpha_1 = SFdata[0,:,:,:].reshape((-1,1))
-    y_values_alpha_2 = SFdata[1,:,:,:].reshape((-1,1))
-
-    y_values = np.hstack((y_values_alpha_1 ,y_values_alpha_2))
-   
+    SFdata_denoised = np.zeros(shape)
+    y_values = yFromSFdata(SFdata)
+    
     results = np.zeros(y_values[:,0].shape)
+    patch_size = 5
+    patch_distance = 6
+    sigma_est = np.mean(estimate_sigma(SFdata[0,:,:,:], multichannel=False))
+    print(sigma_est)
+    patch_kw = dict(patch_size=patch_size,      # 5x5 patches
+                patch_distance=patch_distance)
+   
+    # fast algorithm, sigma provided
+    for i in range(shape[0]):
+        SFdata_denoised[i,:,:,:]  = denoise_nl_means(SFdata[i,:,:,:], h=0.6 * sigma_est, sigma=sigma_est,
+                                    fast_mode=True, **patch_kw)  
+        img = nib.Nifti1Image(np.squeeze(SFdata_denoised[i,:,:,:]), np.eye(4))
+                                
+        nib.save(img, "filter_nlm_patch_size_"+str(patch_size)+"_patch_distance_"+str(patch_distance)+"_img_"+str(i)+".nii")
+    
+    y_values_denoised = yFromSFdata(SFdata_denoised)
+   # y_values = y_values_denoised
     for i in range(len(y_values[:,0])):
-        pop, _ = curve_fit(function_B1,
-                                [init['T1'] ,init['TR']],
-                                y_values[i,:]  )
-        a , results[i] = pop                        
+        x_values =[ init['T1'] ,init['TR']]
+        for k in range(len(y_values[i,:])):
+            x_values.append(y_values[i,k] )
+        try:
+            pop, _ = curve_fit(function_B1_double,
+                                    x_values,
+                                    y_values[i,:]  )
+        
+        except RuntimeError:
+            results[i] = -1  
+            pass
+     #   print(y_values[i,:],results[i])
+        results[i]= pop #
+             
     array = results.reshape((shape[1], shape[2], shape[3]))
-    #  
-    print("Mean",results.mean())
     img = nib.Nifti1Image(np.squeeze(array), np.eye(4))
-    nib.save(img, "result_carte_b1_"+sub+".nii")
+    nib.save(img, "result_carte_b1_"+sub+"_den_patch_size_"+str(patch_size)+"_patch_distance_"+str(patch_distance)+".nii")
+
+    alpha_estim = array[idx_courone[0] , idx_courone[1], idx_courone[2]  ] 
+    s_obs =   SFdata[:,idx_courone[0],idx_courone[1],idx_courone[2] ] 
+    s_obs_deno =   SFdata_denoised[:,idx_courone[0],idx_courone[1],idx_courone[2] ] 
+    print(s_obs, s_obs_deno)
+    alphas = get_sequence_alpha(alpha_estim)
+    s_estimate = equation_B1(init['T1'] ,init['TR'], alphas, 1)
+    den = np.sum(np.power(s_estimate,2))
+    num = np.sum(s_obs*s_estimate)
+    S0 = num/den
+    plt.title('Signal ')
+    plt.xlabel('alpha [°]')
+    plt.plot(alphas, s_obs, 'gx', label='S_measure')
+    plt.plot(alphas, s_obs_deno, 'rx',label='S_measure_den')
+    alpha_fit = np.linspace(1,65,65)
+    plt.plot(alpha_fit,  equation_B1(init['T1'] ,
+                                        init['TR'],
+                                        alpha_fit, 1)*S0,
+                                        'b', label='S_fit')
+    plt.legend()
+    plt.show()
 
 # alpha = np.arange(90)
-# keys_sub = search_keys_sub(init)
+# keys_sub = search_keys_sub(init)#
 
-# theorical_ratio = ratio_equation_b1(alpha, 2*alpha, init['T1'] , init['TR'] )
-# print(theorical_ratio.shape)
-# plt.title('Sensitivity ratio DAM')
+#plt.title('Sensitivity ratio DAM')
 # plt.xlabel('alpha [°]')
 # plt.ylabel('S(alpha)/S(2*alpha)')
-
-# plt.plot(alpha, theorical_ratio)
+#plt.plot(alpha, theorical_ratio)
 # plt.show()
-"""
--- utils_own/b1_mapping.py
+++ utils_own/b1_mapping.py
@@ -14,24 +14,62 @@ from utils_own.model import *
 
 INITIALIZATION = dict()
 
+INITIALIZATION['b1_database_phantom'] = {
+    #   'sub-00' : ['/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-20/meas_MID112_31P_MT_cATP_FA0_PCr_VA12_FID16768_filter_hamming2_freq_0_echo_0.nii',
+ #               '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-20/meas_MID110_31P_MT_cATP_FA0_PCr_VA24_FID16766_filter_hamming2_freq_0_echo_0.nii',
+ #           '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-20/meas_MID103_31P_MT_cATP_FA0_PCr_VA36_FID16759_filter_hamming2_freq_0_echo_0.nii',
+ #           '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-20/meas_MID108_31P_MT_cATP_FA0_PCr_VA48_FID16764_filter_hamming2_freq_0_echo_0.nii'], 
+
+'sub-01' : [
+    '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-29/meas_MID634_31P_MT_cATP_FA0_PCr_VA24_FID17904_filter_hamming2_freq_0_echo_0.nii',
+   '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-29/meas_MID633_31P_MT_cATP_FA0_PCr_VA48_FID17903_filter_hamming2_freq_0_echo_0.nii'], 
+
+    'FA_nominal' : [12,24,36,48],
+    'T1' : 6.7,
+    'TR' : 0.250
+    }
 INITIALIZATION['b1_database'] = {
-    'sub-01' : ['/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-14/meas_MID236_31P_MT_cATP_FA0_PCr_VA50_FID16096_filter_hamming2_freq_0_echo_0.nii',
-                '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Phantom/2021-01-14/meas_MID237_31P_MT_cATP_FA0_PCr_VA25_FID16097_filter_hamming2_freq_0_echo_0.nii'],
-    'FA_nominal' : [25,50],
+ 'sub-01' : [
+    '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Volunteer/2021-01-27/meas_MID468_31P_MT_cATP_FA0_PCr_VA12_FID17680_filter_hamming2_freq_0_echo_0.nii',
+   '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Volunteer/2021-01-27/meas_MID467_31P_MT_cATP_FA0_PCr_VA24_FID17679_filter_hamming2_freq_0_echo_0.nii',
+'/neurospin/ciclops/people/Renata/ReconstructedData/31P_Volunteer/2021-01-27/meas_MID466_31P_MT_cATP_FA0_PCr_VA36_FID17678_filter_hamming2_freq_0_echo_0.nii',
+    '/neurospin/ciclops/people/Renata/ReconstructedData/31P_Volunteer/2021-01-27/meas_MID465_31P_MT_cATP_FA0_PCr_VA48_FID17677_filter_hamming2_freq_0_echo_0.nii'],
+
+    'FA_nominal' : [12,24,36,48],
     'T1' : 3.37,
     'TR' : 0.250,
 }
-
+"""
+def yFromSFdata(SFdata):
+    y_values_alpha_1 = SFdata[0,:,:,:].reshape((-1,1))
+    y_values_alpha_2 = SFdata[1,:,:,:].reshape((-1,1))
+    return np.hstack((y_values_alpha_1 ,y_values_alpha_2))
 def get_sequence_alpha(alpha):
     return np.array([alpha, 2*alpha]).flatten()
+"""
 
-def function_B1_double(x_values, alpha, S0):
+def yFromSFdata(SFdata):
+    y_values_alpha_1 = SFdata[0,:,:,:].reshape((-1,1))
+    y_values_alpha_2 = SFdata[1,:,:,:].reshape((-1,1))
+    y_values_alpha_3 = SFdata[2,:,:,:].reshape((-1,1))
+    y_values_alpha_4 = SFdata[3,:,:,:].reshape((-1,1))
+    return np.hstack((y_values_alpha_1 ,y_values_alpha_2,y_values_alpha_3,y_values_alpha_4))
+def get_sequence_alpha(alpha):
+    return np.array([alpha, 2*alpha, 3*alpha, 4*alpha]).flatten()
+
+def function_B1_double(x_values, alpha):
     T1 = x_values[0]
     TR = x_values[1]
+    y_obs =[]
+    for i in range(len(x_values )-2):
+        y_obs.append(x_values[i+2] ) 
+
     alphas = get_sequence_alpha(alpha)
-    ans = equation_B1(T1, TR, alphas, S0)
-#    S0 = /ans*ans
-    return ans
+    ans = equation_B1(T1, TR, alphas, 1)
+    den = np.sum(np.power(ans,2))
+    num = np.sum(y_obs*ans)
+    S0 = num/den
+    return ans*S0
 
 
 def function_B1(x_values, S0, alpha):
